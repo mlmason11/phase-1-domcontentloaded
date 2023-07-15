2aec35d83f06dd850a743272723f6656a18ffb08
Max <mlmason11@gmail.com>
2023-07-15 01:55:10 -0400

    Submitting domcontentloaded lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+const newText = 'This is really cool!'
+const text = document.getElementById('text')
+
+document.addEventListener('DOMContentLoaded', () => {
+    text.textContent = newText
+})
